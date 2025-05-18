f75eed5db1db8a16f057418d04b9150f813d3a1c
stackloop <squarestacker@gmail.com>
2025-05-18 16:36:45 -0300

    First working prototype

--- hanoi.py
+++ hanoi.py
@@ -10,13 +10,13 @@ class Hanoi(Scene):
         self.size = size
         self.block_height = min(self.stack_height / size, 0.75)
         self.block_max_width = self.stack_distance - 1
+        self.step_time = 1 / size
         super().__init__(**kwargs)
 
     def construct(self):
         self.draw_stacks()
         self.init_blocks()
-        self.move_block(0, 2)
-        self.move_block(0, 2)
+        self.solve(size, 0, 2)
         self.wait()
     
     def draw_stacks(self):
@@ -49,18 +49,30 @@ class Hanoi(Scene):
     
     def move_block(self, from_stack, to_stack):
         block = self.stacks[from_stack][-1]
-        self.play(Transform(block, block.copy().to_edge(UP)), run_time=0.5)
+        self.play(Transform(block, block.copy().to_edge(UP)), run_time=self.step_time)
 
         target_x = (LEFT * self.stack_distance) + (RIGHT * self.stack_distance * to_stack)
         move_x = target_x - block.get_center()[0]
-        self.play(block.animate.shift(RIGHT * move_x), run_time=0.5)
+        self.play(block.animate.shift(RIGHT * move_x), run_time=self.step_time)
 
         self.play(
             Transform(block, block.copy().to_edge(DOWN).shift(UP * self.block_height * len(self.stacks[to_stack]))),
-            run_time=0.5
+            run_time=self.step_time
         )
 
         self.stacks[to_stack].append(self.stacks[from_stack].pop())
+    
+    def solve(self, depth, from_stack, to_stack):
+        if depth == 1:
+            return self.move_block(from_stack, to_stack)
+        
+        buffer = (from_stack + to_stack) ^ 3
+
+        self.solve(depth - 1, from_stack, buffer)
+        self.move_block(from_stack, to_stack)
+        self.solve(depth - 1, buffer, to_stack)
+
+        
 
 def error(message):
     print(message)
@@ -71,8 +83,8 @@ if __name__ == "__main__":
         error("Usage: python hanoi.py <size>")
     try:
         size = int(sys.argv[1])
-        if size < 1 or size > 50:
-            error("Size must be a value in the range 1 - 50.")
+        if size < 1 or size > 8:
+            error("Size must be a value in the range 1 - 8.")
         with tempconfig({"output_file": f"Hanoi_{size}"}):
             Hanoi(size).render()
     except ValueError:
