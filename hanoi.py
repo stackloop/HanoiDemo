8393c2179503c59e23afce4e831e5b31d73b5ca3
stackloop <squarestacker@gmail.com>
2025-05-18 16:13:45 -0300

    Now able to move a single piece

---
+++ hanoi.py
@@ -0,0 +1,76 @@
+from manim import *
+import sys
+import os
+
+class Hanoi(Scene):
+    stack_distance = 4
+    stack_height = 5
+
+    def __init__(self, size, **kwargs):
+        self.size = size
+        self.block_height = min(self.stack_height / size, 0.75)
+        self.block_max_width = self.stack_distance - 1
+        super().__init__(**kwargs)
+
+    def construct(self):
+        self.draw_stacks()
+        self.init_blocks()
+        self.move_block(0, 2)
+        self.wait()
+    
+    def draw_stacks(self):
+        stacks = [
+            RoundedRectangle(
+                width=0.4, height=self.stack_height, corner_radius=0.1
+            ).shift(
+                (LEFT * self.stack_distance) + (RIGHT * self.stack_distance * i)
+            ).to_edge(DOWN)
+            for i in range(3)
+        ]
+        self.play(*(Create(rect) for rect in stacks))
+
+    def init_blocks(self):
+        self.stacks = [[], [], []]
+        step = (self.block_max_width - 1) / max(self.size - 1, 1)
+        for i in range(self.size):
+            self.stacks[0].append(
+                RoundedRectangle(
+                    width=self.block_max_width - i * step,
+                    height = self.block_height,
+                    corner_radius=0.1
+                ).set_fill(
+                    BLACK, opacity=1
+                ).to_edge(DOWN).shift(
+                    LEFT * self.stack_distance + (UP * self.block_height * i)
+                )
+            )
+        self.play(*(Create(block) for block in self.stacks[0]))
+    
+    def move_block(self, from_stack, to_stack):
+        block = self.stacks[from_stack][-1]
+        self.play(Transform(block, block.copy().to_edge(UP)), run_time=0.5)
+
+        target_x = (LEFT * self.stack_distance) + (RIGHT * self.stack_distance * to_stack)
+        move_x = target_x - block.get_center()[0]
+        self.play(block.animate.shift(RIGHT * move_x), run_time=0.5)
+
+        self.play(
+            Transform(block, block.copy().to_edge(DOWN).shift(UP * self.block_height * len(self.stacks[to_stack]))),
+            run_time=0.5
+        )
+
+def error(message):
+    print(message)
+    sys.exit(1)
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        error("Usage: python hanoi.py <size>")
+    try:
+        size = int(sys.argv[1])
+        if size < 1 or size > 50:
+            error("Size must be a value in the range 1 - 50.")
+        with tempconfig({"output_file": f"Hanoi_{size}"}):
+            Hanoi(size).render()
+    except ValueError:
+        error("Size is not an integer.")
